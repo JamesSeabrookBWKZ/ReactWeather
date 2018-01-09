c6b7c7765de3bfa5d430eba641f909945815dac8
MrSlips <james.seabrook@brandworkz.com>
2018-01-09 16:48:13 +0000

    Styled the about page and added a link to portfolio page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,9 @@ var React = require('react');
 var About = (props) => {
 	return(
 		<div>
-			<h3>About</h3>
-			<p>Welcome to the about page!</p>
+			<h1 className="text-center">About</h1>
+			<p>This is an app created by <a href="http://www.jamesseabrook.com" target="_BLANK">myself</a> as a way of learning React.</p>
+			<p>It uses openWeatherMap free API to retrieve the data required.</p>
 		</div>
 	)
 };
