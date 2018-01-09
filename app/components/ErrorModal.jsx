f89beae346331295e80c2a67d54fadd07d649d1d
MrSlips <james.seabrook@brandworkz.com>
2018-01-09 17:54:40 +0000

    Updated the error message to show the actual message from the weather API

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,15 +1,25 @@
 var React = require('react');
 
 var ErrorModal = React.createClass({
+    getDefaultProps: function(){
+        return {
+            title: 'Error'
+        };
+    },
     componentDidMount: function(){
         var modal = new Foundation.Reveal($('#error-modal'));
         modal.open();
     },
+    propTypes: {
+        title: React.PropTypes.string,
+        message: React.PropTypes.string.isRequired
+    },
     render(){
+        var {title, message} = this.props;
         return(
             <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-                <h4>Title</h4>
-                <p>error message</p>
+                <h4>{title}</h4>
+                <p>{message}</p>
                 <p><button className="button hollow" data-close="">Okay</button></p>
             </div>
         );
